16f82b8bccf657fcbbc2077dc086a19d7017b76e
mohammadabusbeih-sudo <mohammad.abusbeih@mail.huji.ac.il>
2026-08-03 09:52:03 +0300

    Delete src/routes/knowledge.hadith.tsx

--- src/routes/knowledge.hadith.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/knowledge/hadith")({
-  beforeLoad: () => {
-    throw redirect({ to: "/hadith" });
-  },
-});
